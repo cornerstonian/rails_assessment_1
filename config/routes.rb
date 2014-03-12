9baabed9a2fc351a61b5e09598d740379e1b7637
Lavoisier Cornerstone <vois.cornerstone@gmail.com>
2014-03-12 02:46:52 -0400

    adds controller, makes associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 RailsAssessment1::Application.routes.draw do
+  resources :posts do
+
+    resources :comments, :only => [:create]
+  end
+
+  root :to => 'posts#index'
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
